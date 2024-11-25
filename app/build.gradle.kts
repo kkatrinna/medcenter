9f0a168b84cf825b06b92921d7f44b8da3c9a520
ekaterina_pronina <ekat.mi3665@mail.ru>
2024-11-25 12:45:24 +0300

    время записи

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
     implementation ("androidx.cardview:cardview:1.0.0")
